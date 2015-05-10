d2c51b60f066e9a515ef266d39f25aa8f673b7f7
Sebastian Ruder <ruder.sebastian@gmail.com>
2015-05-10 12:42:00 +0100

    Another commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,6 +23,9 @@ public class Main extends HttpServlet {
             ClassificationRunner R1 = new ClassificationRunner(userId);
             List<Map.Entry<String, Double>> list = R1.runClassification(); // at the moment run asynchronously
             resp.getWriter().print("400. Successful. User posts have been analyzed. Userid: " + userId + "\n");
+            for (Map.Entry<String, Double> entry : list) {
+                resp.getWriter().print(entry.getKey() + ": " + entry.getValue() + "\n");
+            }
         //}
         //else {
         //    showHome(req,resp);
